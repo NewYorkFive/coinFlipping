ff013cb7deaee2355171d83def551cfaf3cba342
NowOrNever <tobeabetterman_dl@163.com>
2017-05-07 20:08:40 +0800

    add gif

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # coinFlipping
+
+how to use
+	
+	- (void)viewDidLoad {
+	    [super viewDidLoad];
+	    DLFilpView *flipView = [[DLFilpView alloc]initWithFirstImageName:@"front" secondImageName:@"tail" timeInterval:0.2 andDirection:DLFlipDirectionX];
+	    
+	    [self.view addSubview:flipView];
+	    flipView.bounds = CGRectMake(0, 0, 200, 200);
+	    flipView.center = self.view.center;
+	}
+	
--- coinFlipping/coinFlipping/ViewController.m
+++ coinFlipping/coinFlipping/ViewController.m
@@ -16,16 +16,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self setupUI];
-}
-
-- (void)setupUI{
-    DLFilpView *flipView = [[DLFilpView alloc]initWithFirstImageName:@"front" secondImageName:@"tail" timeInterval:0.6 andDirection:DLFlipDirectionX];
-
+    DLFilpView *flipView = [[DLFilpView alloc]initWithFirstImageName:@"front" secondImageName:@"tail" timeInterval:0.2 andDirection:DLFlipDirectionX];
+    
     [self.view addSubview:flipView];
     flipView.bounds = CGRectMake(0, 0, 200, 200);
     flipView.center = self.view.center;
 }
+
 
 
 - (void)didReceiveMemoryWarning {
